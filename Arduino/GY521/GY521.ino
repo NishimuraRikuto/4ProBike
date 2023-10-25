7c3fd35d3ec2695fc2d94c464c5ccc754de20508
nishimurarikuto <nisriku@gmail.com>
2023-10-25 20:03:00 +0900

    add arduino program

---
+++ Arduino/GY521/GY521.ino
@@ -0,0 +1,91 @@
+#include <Joystick.h>
+Joystick_ Joystick;
+#include <Wire.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+
+#define Gyro_X 24
+#define Gyro_Y -50
+#define Gyro_Z 24
+#define Accel_Z 751
+
+MPU6050 mpu;
+static uint8_t mpuIntStatus;
+static bool dmpReady = false;  // set true if DMP init was successful
+static uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+
+int16_t  Gyro_Now = 0, Gyro = 0, Gyro_Offset =0, Gyro_y_Now=0, Gyro_y=0;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64]; // FIFO storage buffer                 // orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  
+  Gryo_Start();
+  Joystick.begin();
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(512);
+  Joystick.setZAxis(512);
+}
+
+void loop() {
+  GyroGet();
+//ButtonGet();
+//Joystick.setButton(Button);
+  Serial.println(Gyro);
+  Serial.println(Gyro_y);
+  Joystick.setYAxis(Gyro);
+  Joystick.setXAxis(Gyro_y+512);
+  
+}
+
+
+void GyroGet() {
+  mpuIntStatus = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    mpu.resetFIFO();
+  }
+  else if (mpuIntStatus & 0x02) {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    fifoCount -= packetSize;
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Gyro_Now = degrees(ypr[1])*10 + 180;//ypr[1]でy軸ypr[2]でx軸
+    Gyro_y_Now=degrees(ypr[2])*10 + 180;
+    Gyro = Gyro_Now + Gyro_Offset-70;
+    Gyro_y=Gyro_y_Now + Gyro_Offset+100;
+//    if (Gyro < 0) Gyro += 360;
+//    if (Gyro > 359) Gyro -= 360;
+//    if (Gyro_y<0) Gyro_y+=360;
+//    if (Gyro_y>359) Gyro_y-=360;
+  }
+}
+
+
+
+void Gryo_Start() {
+  mpu.initialize();
+  if (mpu.testConnection() != true) {
+    Serial.println("MPU disconection");
+    while (true) {}
+  }
+  if (mpu.dmpInitialize() != 0) {
+    Serial.println("MPU break");
+    while (true) {}
+  }
+  mpu.setXGyroOffset(Gyro_X);
+  mpu.setYGyroOffset(Gyro_Y);
+  mpu.setZGyroOffset(Gyro_Z);
+  mpu.setZAccelOffset(Accel_Z);
+  mpu.setDMPEnabled(true);
+  mpuIntStatus = mpu.getIntStatus();
+  dmpReady = true;
+  packetSize = mpu.dmpGetFIFOPacketSize();
+}
